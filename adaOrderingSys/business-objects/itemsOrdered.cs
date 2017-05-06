dea729ba0f8f62e5d1496819531d8c2e4398056d
tjlinton <tj_linton@yahoo.com>
2017-05-06 02:52:21 -0500

    Added Items Ordered Class

---
+++ adaOrderingSys/business-objects/itemsOrdered.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adaOrderingSys.business_objects
+{
+    class itemsOrdered
+    {
+        private int itemID { get; set; }
+        private int orderID { get; set; }
+
+        public itemsOrdered() { }
+    }
+}
